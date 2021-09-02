62fff159f6cf168bf404263f185cfc45af5e56ae
renkexi <89432303+renkexi@users.noreply.github.com>
2021-09-02 11:02:41 +0800

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,27 @@
+#dict={"k1":"v1","k2":"v2","k3":"v3"}
+#dict['k4']='v4'
+#print(dict)
+#print(dict.keys())
+#print(dict.values())
+
+
+#Friuts = {'苹果':12.3,'草莓':4.5,'香蕉':6.3,'葡萄':5.8,'橘子':6.4,'樱桃':15.8}
+#info = {'小明':{'fruits':{'苹果':4,'草莓':13,'香蕉':10},'money':0},'小刚':{'fruits':{'葡萄':19,'橘子':12,'樱桃':30},'money':0}}
+#sum1: float = info['小明']['fruits']['苹果']*Friuts['苹果']+info['小明']['fruits']['草莓']*Friuts['草莓']+info['小明']['fruits']['香蕉']*Friuts['香蕉']
+#sum2 = info['小刚']['fruits']['葡萄']*Friuts['葡萄']+info['小刚']['fruits']['橘子']*Friuts['橘子']+info['小刚']['fruits']['樱桃']*Friuts['樱桃']
+#info['小明']['money'] = sum1
+#info['小刚']['money'] = sum2
+#print(info)
+
+#def num(l):
+   # d={}
+    #for i in l:
+        #if type(i) is int:
+           # if i not in d:
+               # d[i]=1
+            #else:
+               # d[i] += 1
+    #return d
+#l=[21,21,21,59,59,59,59,59,59,59,59,59,10,10,10]
+#print(num(l))
+
